2fd07dae2b23b4cd7e72fcf4be3ad2ee8d6d7045
frh <felix@hjerrild.org>
2022-02-16 11:21:27 -0500

    Added simple page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Velkommen til libertær.dk - En verden af frihed"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
