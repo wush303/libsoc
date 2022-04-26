c027afc79ae1c3f2679028eb1a8203bb6520bb7b
frh <felix@hjerrild.org>
2022-04-26 08:40:08 +0200

    added static

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::form::Form;
+use rocket::fs::{relative, FileServer, NamedFile};
 use rocket::http::Cookie;
 use rocket::http::CookieJar;
+
 use uuid::Uuid;
 
+#[derive(FromForm)]
+struct Person<'r> {
+    r#fname: &'r str,
+    r#lname: &'r str,
+}
+
+#[post("/", data = "<person>")]
+async fn bliv_aktiv(person: Form<Person<'_>>, cookies: &CookieJar<'_>) -> Option<NamedFile> {
+    NamedFile::open("static/bliv_aktiv.html").await.ok()
+}
+
 #[get("/")]
-fn index(cookies: &CookieJar<'_>) -> &'static str {
+async fn index(cookies: &CookieJar<'_>) -> Option<NamedFile> {
     match cookies
         .get("id")
         .map(|crumb| format!("Message: {}", crumb.value()))
@@ -14,10 +28,13 @@ fn index(cookies: &CookieJar<'_>) -> &'static str {
         Some(v) => println!("{}", v),
         None => cookies.add(Cookie::new("id", Uuid::new_v4().to_string())),
     }
-    "Velkommen til libertær.dk - En verden af frihed"
+    NamedFile::open("static/index.html").await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/blivaktiv", routes![bliv_aktiv])
+        .mount("/", routes![index])
+        .mount("/", FileServer::from(relative!("static")))
 }
